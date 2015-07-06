68d6e2902a505dff4b9d9ee215691e9124093626
carlos <cala988@gmail.com>
2015-07-06 16:56:43 +0200

    Primera Pregunta 2 (añadir ficheros que faltaban)

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,15 @@
+//método GET de la pregunta /quizes/question
+exports.question = function(req, res){
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+}
+
+//método GET de la respuesta /quizes/answer
+exports.answer = function(req, res){
+	//Comprobamos si la respuesta es correcta y renderizamos las vistas
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	}	
+	else{
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
+}
