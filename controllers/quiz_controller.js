3a95a7e32c8954530c25a1ab793455b913534275
carlos <cala988@gmail.com>
2015-07-13 22:55:18 +0200

    Modelo (sequelize) y Base de Datos (sqlite3)

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,23 @@
-//método GET de la pregunta /quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
-}
+//Importamos el modelo
+var models = require('../models/models.js');
 
-//método GET de la respuesta /quizes/answer
-exports.answer = function(req, res){
-	//Comprobamos si la respuesta es correcta y renderizamos las vistas
-	if(req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	}	
-	else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
-}
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+  })
+};
+
+// GET /quizes/answer
+exports.answer = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto' });
+    } else {
+      res.render('quizes/answer', { respuesta: 'Incorrecto'});
+    }
+  })
+};
 
 //método GET de la respuesta /author
 exports.author = function(req, res){
