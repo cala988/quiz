83a3f3c39c347e865dffd5aab1655fa1427c0954
carlos <cala988@gmail.com>
2015-07-22 17:50:36 +0200

    Crear Preguntas

--- routes/index.js
+++ routes/index.js
@@ -13,9 +13,11 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load) //autoload :quizId
 
 // Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes',                      	quizController.index);
+router.get('/quizes/:quizId(\\d+)',        	quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/quizes/new', 					quizController.new);
+router.post('/quizes/create', 				quizController.create);
 
 //Ruta para los creditos
 router.get('/author', 	quizController.author);
