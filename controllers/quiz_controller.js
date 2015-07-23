7110b5d98b0c16ee827d6ab2a875f8fa7c51437c
carlos <cala988@gmail.com>
2015-07-23 16:54:37 +0200

    Validación de entradas
    Este código no es igual al propuesto en los ejercicios del curso debido
    a problemas con sequelize.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -32,13 +32,13 @@ exports.index = function(req, res) {
 		where: ["UPPER(pregunta) LIKE ?", buscar.toUpperCase()], 
 		order: [['pregunta',  'ASC']]}
 	).then(function(quizes) {
-		res.render('quizes/index', { quizes: quizes});
+		res.render('quizes/index', { quizes: quizes, errors: []});
 	}).catch(function(error)  {next(error);})
     
   }else {
 	models.Quiz.findAll().then(
         function(quizes) {
-			res.render('quizes/index', { quizes: quizes});
+			res.render('quizes/index', { quizes: quizes, errors: []});
 		}
       ).catch(function(error){ next(error); })
   }
@@ -46,7 +46,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -55,7 +55,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -64,20 +64,34 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
+		
+	var errors=quiz.validate();
+	if(errors){
+	/*	
+for (var prop in errors) {
+	if (errors.hasOwnProperty(prop))
+		console.log("Errors for field " + prop + ": ");
+	for (var i=0; i<errors[prop].length; ++i) {
+		console.log("\t" + errors[prop]);
+	}
+}*/
 
-// guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');  
-  })   // res.redirect: Redirección HTTP a lista de preguntas
+		res.render('quizes/new', {quiz: quiz, errors: errors});
+	  } else {
+		quiz // save: guarda en DB campos pregunta y respuesta de quiz
+		.save({fields: ["pregunta", "respuesta"]})
+		.then( function(){ res.redirect('/quizes')}) 
+	  }      // res.redirect: Redirección HTTP a lista de preguntas
+    
 };
 
 //método GET de la respuesta /author
 exports.author = function(req, res){
-	res.render('author', {nombre: 'Senor X', srcImagen:'/images/senorX.png'});
+	res.render('author', {nombre: 'Senor X', srcImagen:'/images/senorX.png', errors: []});
 }
