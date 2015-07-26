97ea7e01f1ac4c3420be7f4fca4eb51c69db39d9
carlos <cala988@gmail.com>
2015-07-26 19:27:50 +0200

    Añadir columna tema. Ejercicio P2P modulo 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -75,8 +75,8 @@ exports.create = function(req, res) {
 	if(errors){
 		res.render('quizes/new', {quiz: quiz, errors: errors});
 	  } else {
-		quiz // save: guarda en DB campos pregunta y respuesta de quiz
-		.save({fields: ["pregunta", "respuesta"]})
+		quiz // save: guarda en DB campos pregunta, respuesta y tema de quiz
+		.save({fields: ["pregunta", "respuesta", "tema"]})
 		.then( function(){ res.redirect('/quizes')}) 
 	  }      // res.redirect: Redirección HTTP a lista de preguntas
     
@@ -98,8 +98,8 @@ exports.update = function(req, res) {
 	if(errors){
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
     } else {
-        req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
+        req.quiz     // save: guarda campos pregunta, respuesta y tema en DB
+        .save( {fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes');});
     }     // Redirección HTTP a lista de preguntas (URL relativo)
   
