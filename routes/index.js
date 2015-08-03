102793a7cdb951f59f85a797f7b87a83cf4c976e
carlos <cala988@gmail.com>
2015-08-03 21:12:40 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load) //autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -33,6 +34,8 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 //Para los comentarios:
 router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
 
 //Ruta para los creditos
 router.get('/author', 	quizController.author);
